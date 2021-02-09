43ecb9058055ab9f514d26ce29ad1ca032ec994e
wookyoungkim <viliketh1s98@naver.com>
2021-02-09 15:59:26 +0900

    Initial commit

---
+++ src/main/java/kr/carz/savecar/SavecarApplication.java
@@ -0,0 +1,13 @@
+package kr.carz.savecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavecarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/carz/savecar/SavecarApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.carz.savecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavecarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
