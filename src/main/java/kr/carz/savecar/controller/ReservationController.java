78be6a1faeb77ef6038d5cbec032b052881f9a56
wookyoungkim <viliketh1s98@naver.com>
2021-02-20 21:52:16 +0900

    예약 조회 api 구현 #18

--- src/main/java/kr/carz/savecar/controller/ReservationController.java
+++ src/main/java/kr/carz/savecar/controller/ReservationController.java
@@ -1,15 +1,20 @@
 package kr.carz.savecar.controller;
 
+import kr.carz.savecar.domain.CampingCar;
+import kr.carz.savecar.domain.Reservation;
 import kr.carz.savecar.domain.ReservationSaveDto;
 import kr.carz.savecar.service.MonthlyRentService;
 import kr.carz.savecar.service.ReservationService;
 import kr.carz.savecar.service.YearlyRentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class ReservationController {
@@ -22,7 +27,10 @@ public class ReservationController {
 
     @GetMapping("/reservation/list")
     @ResponseBody
-    public String reservation_list() {
+    public String reservation_list(Model model) {
+        List<Reservation> reservationList = reservationService.findAllReservations();
+        model.addAttribute("reservationList", reservationList);
+
         return "reservation_list";
     }
 
