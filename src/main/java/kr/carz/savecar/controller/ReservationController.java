75690d48943c42df323987073914bfe17b432602
wookyoungkim <viliketh1s98@naver.com>
2021-02-22 17:10:13 +0900

    메세지 전송 기능 구현 close #11

--- src/main/java/kr/carz/savecar/controller/ReservationController.java
+++ src/main/java/kr/carz/savecar/controller/ReservationController.java
@@ -6,6 +6,9 @@ import kr.carz.savecar.domain.ReservationSaveDto;
 import kr.carz.savecar.service.MonthlyRentService;
 import kr.carz.savecar.service.ReservationService;
 import kr.carz.savecar.service.YearlyRentService;
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -38,6 +42,27 @@ public class ReservationController {
     @PostMapping("/reservation/apply")
     @ResponseBody
     public Long save(@RequestBody ReservationSaveDto dto){
+
+        String api_key = "NCS0P5SFAXLOJMJI";
+        String api_secret = "FLLGUBZ7OTMQOXFSVE6ZWR2E010UNYIZ";
+        Message coolsms = new Message(api_key, api_secret);
+        HashMap<String, String> params = new HashMap<String, String>();
+
+        params.put("to", "01058283328");
+        params.put("from", "01058283328");
+        params.put("type", "SMS");
+        params.put("text", dto.getName() + "님이 " + dto.getCategory1() + " 예약을 완료하였습니다." +
+                "\n" + "예약확인 바로가기 : http://itscar.cafe24.com//reservation/list"); //메시지 내용
+        params.put("app_version", "test app 1.2");
+
+        try {
+            JSONObject obj = (JSONObject) coolsms.send(params);
+            System.out.println(obj.toString()); //전송 결과 출력
+        } catch (CoolsmsException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getCode());
+        }
+
         return reservationService.save(dto);
     }
 }
