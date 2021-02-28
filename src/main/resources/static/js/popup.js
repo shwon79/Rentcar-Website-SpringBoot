874c9911b087d974c5ce2264c977a0af7ac99d27
leejieuns2 <2jjingni@gmail.com>
2021-02-28 17:16:27 +0900

    fix popup settings

--- src/main/resources/static/js/popup.js
+++ src/main/resources/static/js/popup.js
@@ -1,26 +1,7 @@
-function setCookie(name, value, expiredays) {
-    var todayDate = new Date();
-    todayDate.setDate(todayDate.getDate() + expiredays);
-    document.cookie = name + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";"
-}
-
 function closeWin1() {
-    if (document.notice_form1.chkbox1.checked) {
-        setCookie("maindiv", "done", 1);
-    }
-    document.all['divpop1'].style.visibility = "hidden";
+    document.getElementById('divpop1').style.visibility = "hidden";
 }
 
 function closeWin2() {
-    if (document.notice_form2.chkbox2.checked) {
-        setCookie("maindiv", "done", 1);
-    }
-    document.all['divpop2'].style.visibility = "hidden";
-}
-
-cookiedata = document.cookie;
-if (cookiedata.indexOf("maindiv=done") < 0) {
-    document.all['divpop'].style.visibility = "visible";
-} else {
-    document.all['divpop'].style.visibility = "hidden";
+    document.getElementById('divpop2').style.visibility = "hidden";
 }
