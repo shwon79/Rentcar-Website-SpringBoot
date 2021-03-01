c65618bb1df4ec9e75b98fe239ce9037ba45a181
leejieuns2 <2jjingni@gmail.com>
2021-03-02 00:59:21 +0900

    fix camping-car images and popup settings

--- src/main/resources/static/js/popup.js
+++ src/main/resources/static/js/popup.js
@@ -5,3 +5,7 @@ function closeWin1() {
 function closeWin2() {
     document.getElementById('divpop2').style.visibility = "hidden";
 }
+
+function closeWin3() {
+    document.getElementById('divpop3').style.visibility = "hidden";
+}
