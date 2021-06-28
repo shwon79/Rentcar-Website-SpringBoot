ed37685459a46903880e7a9928f2e8cf65fd1554
sne12345@github.com <skdms0427!>
2021-06-28 14:05:27 +0900

    0628

--- src/main/resources/static/js/popup.js
+++ src/main/resources/static/js/popup.js
@@ -4,13 +4,28 @@ function setCookie( name, value, expiredays ) {
     var todayDate = new Date();
     todayDate.setDate( todayDate.getDate() + expiredays );
     document.cookie = name + "=" + escape( value ) + "; path=/; expires=" + todayDate.toGMTString() + ";"
-
+    console.log(todayDate.getDate())
 }
+
+// 00:00 시 기준 쿠키 설정하기
+// expiredays 의 새벽  00:00:00 까지 쿠키 설정
+function setCookieAt00( name, value, expiredays ) {
+    var todayDate = new Date();
+    todayDate = new Date(parseInt(todayDate.getTime() / 86400000) * 86400000 + 54000000);
+    if ( todayDate > new Date() )
+    {
+        expiredays = expiredays - 1;
+    }
+    todayDate.setDate( todayDate.getDate() + expiredays );
+    document.cookie = name + "=" + escape( value ) + "; path=/; expires=" + todayDate.toGMTString() + ";"
+    console.log(document.cookie)
+}
+
 
 function closeWinOne() {
 
     if ( document.pop_form1.chkbox.checked ) {
-        setCookie( "maindiv1", "done" , 1 );
+        setCookieAt00( "maindiv1", "done" , 1 );
         console.log('if pass')
     }
 
@@ -20,7 +35,7 @@ function closeWinOne() {
 function closeWinTwo() {
 
     if ( document.pop_form2.chkbox.checked ) {
-        setCookie( "maindiv2", "done" , 1 );
+        setCookieAt00( "maindiv2", "done" , 1 );
         console.log('if pass')
     }
 
@@ -30,7 +45,7 @@ function closeWinTwo() {
 function closeWinThree() {
 
     if ( document.pop_form3.chkbox.checked ) {
-        setCookie( "maindiv3", "done" , 1 );
+        setCookieAt00( "maindiv3", "done" , 1 );
         console.log('if pass')
     }
 
@@ -41,7 +56,7 @@ function closeWinThree() {
 function closeWinFour() {
 
     if ( document.pop_form4.chkbox.checked ) {
-        setCookie( "maindiv4", "done" , 1 );
+        setCookieAt00( "maindiv4", "done" , 1 );
         console.log('if pass')
     }
 
