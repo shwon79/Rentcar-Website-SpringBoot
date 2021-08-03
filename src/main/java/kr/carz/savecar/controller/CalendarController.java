b14b4df36b913ae2910b82b0939e2454c5ee8c2b
Naeun Song <sne12345@naver.com>
2021-08-03 11:36:20 +0900

    0803-달력 넘기기 기능

--- src/main/java/kr/carz/savecar/controller/CalendarController.java
+++ src/main/java/kr/carz/savecar/controller/CalendarController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -56,6 +59,7 @@ public class CalendarController {
         Calendar cal = Calendar.getInstance();
 
         int now_month = cal.get(Calendar.MONTH)+1;
+        int now_year = cal.get(Calendar.YEAR);
 
         List<CalendarDate> calendarDateList = calendarDateService.findCalendarDateByMonth(Integer.toString(now_month));
 
@@ -94,10 +98,75 @@ public class CalendarController {
 
         System.out.println(dateCampingList.get(0).get(0).getCarName());
         model.addAttribute("dateCampingList", dateCampingList);
+        model.addAttribute("thisMonth", now_month);
+        model.addAttribute("thisYear", now_year);
 
 
         return "calendar";
     }
+
+
+    @RequestMapping("/calendar/{year}/{month}")
+    public String handleRequest1(HttpServletRequest request, HttpServletResponse response, ModelMap model, @PathVariable("year") Long year, @PathVariable("month") Long month) throws Exception {
+        // 날짜
+        Calendar cal = Calendar.getInstance();
+
+        int now_month = month.intValue()+1; // 9
+
+        if(now_month > 11){
+            response.setContentType("text/html; charset=UTF-8");
+
+            PrintWriter out = response.getWriter();
+
+            out.println("<script>alert('이용할 수 없는 예약일자입니다.'); location.href='/calendar/10';</script>");
+
+            out.flush();
+        } else {
+
+            List<CalendarDate> calendarDateList = calendarDateService.findCalendarDateByMonth(Long.toString(now_month));
+
+
+            // 전달 날짜 구하기
+            cal.set(cal.get(Calendar.YEAR), now_month - 1, 1); // 8
+
+            Long firstDateId = calendarDateList.get(0).getDateId();
+            Integer before = cal.get(Calendar.DAY_OF_WEEK);
+
+            for (int i = 1; i < before; i++) {
+                calendarDateList.add(0, calendarDateService.findCalendarDateByDateId(firstDateId - i));
+            }
+
+            // 다음달 날짜 구하기
+            cal.set(cal.get(Calendar.YEAR), now_month - 1, Integer.parseInt(calendarDateList.get(calendarDateList.size() - 1).getDay()));
+
+            Long lastDateId = calendarDateList.get(calendarDateList.size() - 1).getDateId();
+            Integer after = cal.get(Calendar.DAY_OF_WEEK);
+
+            for (int i = 1; i <= 7 - after; i++) {
+                calendarDateList.add(calendarDateService.findCalendarDateByDateId(lastDateId + i));
+            }
+
+            model.addAttribute("calendarDateList", calendarDateList);
+
+
+            // 날짜별 캠핑카
+            List<List<DateCamping>> dateCampingList = new ArrayList();
+
+            for (int i = 0; i < calendarDateList.size(); i++) {
+                dateCampingList.add(dateCampingService.findByDateId(calendarDateList.get(i)));
+            }
+
+            System.out.println(dateCampingList.get(0).get(0).getCarName());
+            model.addAttribute("dateCampingList", dateCampingList);
+            model.addAttribute("thisMonth", now_month);
+            model.addAttribute("thisYear", now_month);
+        }
+
+
+        return "calendar";
+    }
+
+
 
     @GetMapping("/europe_reserve")
     public String camping_europe_reserve(Model model) {
