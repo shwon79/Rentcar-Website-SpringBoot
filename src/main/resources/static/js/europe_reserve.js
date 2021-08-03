88e1d98c8e04848dee506291ca2c5e20bb2eccde
김진현 <rlawlsgus97@gmail.com>
2021-08-03 14:15:16 +0900

    changes on europe reserve.js

--- src/main/resources/static/js/europe_reserve.js
+++ src/main/resources/static/js/europe_reserve.js
@@ -75,13 +75,13 @@ const returnDaySelected = () => {
 }
  */
 
-let rentDateNum;
+let rentDateNum='';
 const rentDate = (id) => {
     rentDateNum = id;
     console.log(id);
 }
 
-let returnDateNum;
+let returnDateNum='';
 const returnDate = (id) => {
     returnDateNum = id;
     console.log(id)
@@ -114,107 +114,23 @@ const rentTimeSel = (id) => {
     rentTime = id;
 }
 
+let prom1 = '';
+let prom2 = '';
+let prom3 = '';
+const getData = async () => {
+    prom1 = await rentDateNum;
+    prom2 = await rentTime;
+    prom3 = await returnDateNum;
+}
 
 let finalDate = {
-    rentDate: rentDateNum,
-    rentTime: rentTime,
-    returnDate: returnDateNum,
+    'rentDate': prom1,
+    'rentTime':  prom2,
+    'returnDate': prom3,
 }
 
 const postDate = () => {
-    console.log(finalDate);
-    /*
-    let url = '';
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(finalDate),
-        headers:{
-            'Content-Type' : 'application/json'
-        }
-    }).then(res => res.json())
-        .then(response => console.log('Success: ', JSON.stringify(response)))
-        .catch(err => console.error('Error: ', err))
-
-     */
-}
-
-
-
-let today = new Date();
-let todayFull = today.toLocaleDateString();
-let todayDay = '';
-switch (today.getDay()) {
-        case 0 : todayDay = '일';
-        case 1 : todayDay = '월';
-        case 2 : todayDay = '화';
-        case 3 : todayDay = '수';
-        case 4 : todayDay = '목';
-        case 5 : todayDay = '금';
-        case 6 : todayDay = '토';
-}
-
-document.getElementById('rent_date').innerText = `${todayFull}(${todayDay})`;
-
-/*
-let rentDay = 0;
-let returnDay = 0;
-const rentDaySelected = () => {
-    rentDay = 1;
-}
-
-const returnDaySelected = () => {
-    returnDay = 1;
-}
- */
-
-let rentDateNum;
-const rentDate = (id) => {
-    rentDateNum = id;
-    console.log(id);
-}
-
-let returnDateNum;
-const returnDate = (id) => {
-    returnDateNum = id;
-    console.log(id)
-}
-
-
-const calculateDate = () => {
-    let date1 = rentDateNum.split('/');
-    let date2 = returnDateNum.split('/');
-    let month1 = date1[0];
-    let day1 = date1[1];
-    let month2 = date2[0];
-    let day2 = date2[1];
-    let monthDiffer = month2-month1;
-    let dayDiffer = day2 - day1;
-    let dateDiffer = 0;
-    if(monthDiffer > 0) {
-        if (dayDiffer > 0) dateDiffer = monthDiffer+1;
-        else if (dateDiffer <= 0) dateDiffer = monthDiffer;
-    } else if(monthDiffer == 0) dateDiffer = 1;
-
-    console.log(dateDiffer);
-    document.getElementById('calResult').innerText = `${dateDiffer}달`;
-}
-
-// time select onclick
-let rentTime = '';
-const rentTimeSel = (id) => {
-    console.log(id);
-    rentTime = id;
-}
-
-
-let finalDate = {
-    rentDate: rentDateNum,
-    rentTime: rentTime,
-    returnDate: returnDateNum,
-}
-
-const postDate = () => {
-    console.log(finalDate);
+    console.log(JSON.stringify(finalDate));
     /*
     let url = '';
     fetch(url, {
