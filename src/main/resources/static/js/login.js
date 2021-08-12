72592a3a3c7c7c73c6786368eb651ceded874b9f
김진현 <rlawlsgus97@gmail.com>
2021-08-12 10:59:40 +0900

    admin, login

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,47 @@
+
+
+let idData = '';
+const inputId = document.getElementById('input_id');
+inputId.addEventListener('change',
+    function () {
+        idData = inputId.value;
+        console.log(inputId.value);
+    })
+
+let pwdData = '';
+const inputPwd = document.getElementById('input_pwd');
+inputPwd.addEventListener('change',
+    function () {
+        pwdData = inputPwd.value;
+        console.log(inputPwd.value);
+    })
+
+const logIn = () => {
+
+    if(idData != '' && pwdData != '') {
+        const url = '/logininfo'
+        const loginData = {
+            "id": idData,
+            "pwd": pwdData,
+        }
+
+        fetch(url, {
+            method: 'POST',
+            headers:{
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(loginData),
+
+        }).then(response => console.log('Success: ', JSON.stringify(response)))
+        /*
+        .then(()=> {
+            if () {
+
+            } else {
+
+                }
+        })
+        */
+    } else alert('입력을 완료해주세요!')
+
+}
