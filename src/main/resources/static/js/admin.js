3ece611c44bf947739013d9638128ad7c703a554
Jay-JYKim91 <jay.jykim91@gmail.com>
2021-11-02 15:00:50 +0900

    post done인데 데이터는 null 상태

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,42 +1,69 @@
+// 할인 적용하기
 
-// 할인 적용하기 -> 수정필요
-function make_discount () {
-
-    if (document.getElementById("reservation-detail-name").value == ""){
-        alert('성함을 입력해주세요.')
+const make_discount = () => {
+    if (document.getElementById("carNo").value == ""){
+        alert('차량 번호를 입력해주세요.')
         return
     }
 
-    if (document.getElementById("reservation-detail-phone").value == ""){
-        alert('전화번호를 입력해주세요.')
+    if (document.getElementById("discount").value == ""){
+        alert('할인 가격을 입력해주세요.')
         return
     }
-
 
     var data = {
-        name : $("#reservation-detail-name").val(),
-        phoneNo : $("#reservation-detail-phone").val(),
-        detail : $("#reservation-detail-details").val(),
-        title : "간편상담신청",
-        car_name : $("#reservation-detail-carname").val(),
-        mileage : $("#reservation-detail-region").val(),
-        option : $("#reservation-detail-resdate").val()
+        carNo : $("#carNo").val(),
+        discount : $("#discount").val()
     };
+    console.log(data);
 
-    var checkbox = document.getElementById("agree")
-    if(checkbox.checked) {
+    $('#discountBtn').click(function(){
         $.ajax({
-            type : 'POST',
-            url : '/reservation/apply',
-            dataType : 'json',
+            type:'post',
+            url:'/admin/discount',
+            dataType:'json',
             contentType : 'application/json; charset=utf-8',
             data : JSON.stringify(data)
         }).done(function () {
-            alert('예약이 완료되었습니다.');
+            alert('할인 가격이 적용되었습니다.');
         }).fail(function (error) {
             alert(JSON.stringify(error));
         })
-    } else{
-        alert("개인정보 수집 및 이용에 동의해주세요.");
-    }
+    });
 }
+
+// function make_discount () {
+//
+//     if (document.getElementById("carNo").value == ""){
+//         alert('차량 번호를 입력해주세요.')
+//         return
+//     }
+//
+//     if (document.getElementById("discount").value == ""){
+//         alert('할인 가격을 입력해주세요.')
+//         return
+//     }
+//
+//
+//     var data = {
+//         carNo : $("#carNo").val(),
+//         discount : $("#discount").val()
+//     };
+//
+//     $(document).ready(function(){
+//         $('#discountBtn').click(function(){
+//             $.ajax({
+//                 type:'post',
+//                 url:'/admin/discount',
+//                 dataType:'json',
+//                 contentType : 'application/json; charset=utf-8',
+//                 data : JSON.stringify(data)
+//             }).done(function () {
+//                 alert('할인 가격이 적용되었습니다.');
+//             }).fail(function (error) {
+//                 alert(JSON.stringify(error));
+//             })
+//         });
+//     });
+//
+// }
