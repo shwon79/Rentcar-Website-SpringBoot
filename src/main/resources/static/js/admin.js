1e1fd784cb4b62313865a1975776114b2a348f1a
Jay-JYKim91 <jay.jykim91@gmail.com>
2021-11-02 19:05:12 +0900

    Admin 할인 적용 차량 리스트

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -30,38 +30,66 @@ const make_discount = () => {
     })
 }
 
-// function make_discount () {
-//
-//     if (document.getElementById("carNo").value == ""){
-//         alert('차량 번호를 입력해주세요.')
-//         return
-//     }
-//
-//     if (document.getElementById("discount").value == ""){
-//         alert('할인 가격을 입력해주세요.')
-//         return
-//     }
-//
-//
-//     var data = {
-//         carNo : $("#carNo").val(),
-//         discount : $("#discount").val()
-//     };
-//
-//     $(document).ready(function(){
-//         $('#discountBtn').click(function(){
-//             $.ajax({
-//                 type:'post',
-//                 url:'/admin/discount',
-//                 dataType:'json',
-//                 contentType : 'application/json; charset=utf-8',
-//                 data : JSON.stringify(data)
-//             }).done(function () {
-//                 alert('할인 가격이 적용되었습니다.');
-//             }).fail(function (error) {
-//                 alert(JSON.stringify(error));
-//             })
-//         });
-//     });
-//
-// }
+// 할인 수정하기
+
+const update_discount = () => {
+    // if (document.getElementById("carNo").value == ""){
+    //     alert('차량 번호를 입력해주세요.')
+    //     return
+    // }
+    //
+    // if (document.getElementById("discount").value == ""){
+    //     alert('할인 가격을 입력해주세요.')
+    //     return
+    // }
+    //
+    // var data = {
+    //     carNo : $("#carNo").val(),
+    //     discount : $("#discount").val()
+    // };
+    // console.log(data);
+    //
+    // $.ajax({
+    //     type:'post',
+    //     url:'/admin/discount',
+    //     dataType:'json',
+    //     contentType : 'application/json; charset=utf-8',
+    //     data : JSON.stringify(data)
+    // }).done(function () {
+    //     alert('할인 가격이 적용되었습니다.');
+    // }).fail(function (error) {
+    //     alert(JSON.stringify(error));
+    // })
+}
+
+// 할인 삭제하기
+
+const delete_discount = () => {
+    // if (document.getElementById("carNo").value == ""){
+    //     alert('차량 번호를 입력해주세요.')
+    //     return
+    // }
+    //
+    // if (document.getElementById("discount").value == ""){
+    //     alert('할인 가격을 입력해주세요.')
+    //     return
+    // }
+    //
+    // var data = {
+    //     carNo : $("#carNo").val(),
+    //     discount : $("#discount").val()
+    // };
+    // console.log(data);
+    //
+    // $.ajax({
+    //     type:'post',
+    //     url:'/admin/discount',
+    //     dataType:'json',
+    //     contentType : 'application/json; charset=utf-8',
+    //     data : JSON.stringify(data)
+    // }).done(function () {
+    //     alert('할인 가격이 적용되었습니다.');
+    // }).fail(function (error) {
+    //     alert(JSON.stringify(error));
+    // })
+}
