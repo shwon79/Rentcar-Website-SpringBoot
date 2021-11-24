888d7398d7ca22cbe1804aee3dae033dfc26575f
Jay-JYKim91 <jay.jykim91@gmail.com>
2021-11-24 15:16:23 +0900

    견적서 틀 및 기본 내용 채우기 완료

--- src/main/resources/static/js/estimate_form.js
+++ src/main/resources/static/js/estimate_form.js
@@ -1,3 +1,15 @@
-document.getElementById('displayToday').addEventListener('click', () => {
-    console.log('Hi');
-})
+//숫자 사이에 콤마 넣기
+let number = document.getElementsByClassName("number");
+
+function numberWithCommas() {
+    for (let i = 0; i < number.length; i++) {
+        if (number[i].innerText === '상담') {
+            number[i].innerText = '상담';
+        } else {
+            const numberWithComma = parseInt(number[i].innerText).toLocaleString();
+            number[i].innerText = numberWithComma;
+        }
+    }
+}
+
+$('.number').ready(numberWithCommas());
