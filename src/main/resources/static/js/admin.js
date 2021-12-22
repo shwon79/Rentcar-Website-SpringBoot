e6b0108d80545f905fb7ee930f5bfe74729673ea
Jay-JYKim91 <jay.jykim91@gmail.com>
2021-12-22 17:22:52 +0900

    캠핑카 관련 정보들 DB에 넣기

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -390,18 +390,33 @@ $('.reservation-completely-delete-btn').click(function(e) {
     }
 });
 
-// let editedText = document.getElementById('editedText');
-// let editableTextList = editedText.split('<br>');
-// editedText.innerText = editableTextList;
-
-
+// setting에서 캠핑카 내용 수정 버튼
 let saveBtn = document.getElementById('saveBtn');
 saveBtn.addEventListener('click', () => {
-    let editedText = document.getElementById('editedText');
-    // let camper_price = editedText.value.replace(/\n/g,'<br>');
+    let editedCamperPrice = document.getElementById('editedCamperPrice');
+    let editedRentPolicy = document.getElementById('editedRentPolicy');
+    let editedRentInsurance = document.getElementById('editedRentInsurance');
+    let editedRentRule = document.getElementById('editedRentRule');
+    let editedRefundPolicy = document.getElementById('editedRefundPolicy');
+    let editedEuropeBasicOption = document.getElementById('editedEuropeBasicOption');
+    let editedEuropeFacility = document.getElementById('editedEuropeFacility');
+    let editedLimousineBasicOption = document.getElementById('editedLimousineBasicOption');
+    let editedLimousineFacility = document.getElementById('editedLimousineFacility');
+    let editedTravelBasicOption = document.getElementById('editedTravelBasicOption');
+    let editedTravelFacility = document.getElementById('editedTravelFacility');
 
     let data = {
-        camper_price: editedText.value
+        camper_price: editedCamperPrice.value,
+        rent_policy: editedRentPolicy.value,
+        rent_insurance: editedRentInsurance.value,
+        rent_rule: editedRentRule.value,
+        refund_policy: editedRefundPolicy.value,
+        europe_basic_option: editedEuropeBasicOption.value,
+        limousine_basic_option: editedLimousineBasicOption.value,
+        travel_basic_option: editedTravelBasicOption.value,
+        europe_facility: editedEuropeFacility.value,
+        limousine_facility: editedLimousineFacility.value,
+        travel_facility: editedTravelFacility.value
     }
     console.log(data);
 
