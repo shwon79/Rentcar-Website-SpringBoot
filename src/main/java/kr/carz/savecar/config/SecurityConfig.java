c3c0c5f8d4be6621581a2ea160923c9f4ee0cfeb
sne12345 <sne12345@naver.com>
2022-01-11 14:49:55 +0900

    관리자 "/admin/index", "/admin/signup", "/admin/login" 제외 다른 것들은 다 관리자권한 필요

--- src/main/java/kr/carz/savecar/config/SecurityConfig.java
+++ src/main/java/kr/carz/savecar/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/admin/index", "/admin/signup", "/admin/login").permitAll()
+                .antMatchers("/admin/campingcar/**", "/admin/counsel/**", "/admin/discount/**", "/admin/moren/**", "/admin/setting/**").hasRole("ADMIN") // ADMIN만 접근 가능
                 .and()
                 .formLogin()     // 로그인 설정
                 .loginPage("/admin/login")      // 커스텀 login 페이지를 사용
