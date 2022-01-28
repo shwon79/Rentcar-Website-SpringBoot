2ab8a2bac7b5e4dfcc001970d715cee2c75b5b23
Jay-JYKim91 <jay.jykim91@gmail.com>
2022-01-28 19:01:41 +0900

    캠핑카 이미지 수정 시작_PUT 구현 중

--- src/main/resources/static/js/admin_campingcar_image.js
+++ src/main/resources/static/js/admin_campingcar_image.js
@@ -1,32 +1,84 @@
-let sec9 = document.querySelector('#europe');
-let imgUploadBtn = sec9.querySelector('.imgUploadBtn');
-let inputFile = sec9.querySelector('input[type="file"]');
-let uploadBox = sec9.querySelector('.upload-box');
+let imgBoxList = document.getElementsByClassName('image-box');
+let uploadBoxList = document.getElementsByClassName('upload-box');
+let inputFileList = document.querySelectorAll('input[type="file"]');
 
-/* 박스 안에 Drag 들어왔을 때 */
-uploadBox.addEventListener('dragenter', function(e) {
-    console.log('dragenter');
-});
+// Array.from(uploadBoxList).forEach(function(uploadBox) {
+//     uploadBox.addEventListener('dragenter', function(e) {
+//         console.log('dragenter');
+//     });
+//
+//     uploadBox.addEventListener('dragover', function(e) {
+//         e.preventDefault();
+//         console.log('dragover');
+//
+//         this.style.backgroundColor = 'lightblue';
+//     });
+//     uploadBox.addEventListener('dragleave', function(e) {
+//         console.log('dragleave');
+//
+//         this.style.backgroundColor = 'white';
+//     });
+//     uploadBox.addEventListener('drop', function(e) {
+//         e.preventDefault();
+//
+//         console.log('drop');
+//         this.style.backgroundColor = 'white';
+//         console.dir(e.dataTransfer);
+//
+//         var data = e.dataTransfer.files[0];
+//         console.dir(data);
+//     });
+// });
 
-/* 박스 안에 Drag를 하고 있을 때 */
-uploadBox.addEventListener('dragover', function(e) {
-    e.preventDefault();
-    console.log('dragover');
 
-    this.style.backgroundColor = 'lightblue';
-});
+// 사진 업로드 버튼
+function uploadImage(carName, title, imageId) {
+    let input = Array.from(inputFileList).filter(ele => ele.title == carName).find(ele => ele.dataset.title == title).files[0];
+    let formData;
 
-/* 박스 밖으로 Drag가 나갈 때 */
-uploadBox.addEventListener('dragleave', function(e) {
-    console.log('dragleave');
+    if (input) {
+        formData = new FormData();
+        formData.append('data', input);
+    }
 
-    this.style.backgroundColor = 'white';
-});
+    let data = {
+        imageId: imageId,
+        carName: carName,
+        title: title,
+        url: '',
+        isUploaded: 1,
+        file: formData
+    };
 
-/* 박스 안에서 Drag를 Drop했을 때 */
-uploadBox.addEventListener('drop', function(e) {
-    e.preventDefault();
+    console.log(data);
 
-    console.log('drop');
-    this.style.backgroundColor = 'white';
-});
+    if (formData) {
+        if (confirm('해당 사진을 업로드 하시겠습니까?')) {
+            $.ajax({
+                enctype: 'multipart/form-data',
+                cache: false,
+                type: 'PUT',
+                url: '/admin/campingcar/image/' + imageId,
+                processData:false,
+                contentType: false,
+                dataType: 'json',
+                data: JSON.stringify(data)
+            }).done(function (result) {
+                if (result.result == 1) {
+                    alert('업로드가 완료되었습니다.');
+                } else {
+                    alert('업로드에 문제가 생겼습니다.');
+                };
+                window.location.href = '/admin/campingcar/image/menu';
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            })
+        };
+    } else {
+        alert('업로드할 사진을 선택해주세요.');
+    }
+
+}
+// function doNotMove() {
+//     window.location = "/admin/campingcar/image/menu";
+// }
