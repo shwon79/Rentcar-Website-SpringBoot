f8ba37e66ef8738308ada8322cab7052d05b6e3c
Jay-JYKim91 <jay.jykim91@gmail.com>
2022-02-03 18:04:16 +0900

    캠핑카 이미지 수정 시작_메인 이미지 post result 오류

--- src/main/resources/static/js/admin_campingcar_image.js
+++ src/main/resources/static/js/admin_campingcar_image.js
@@ -32,51 +32,143 @@ let inputFileList = document.querySelectorAll('input[type="file"]');
 
 
 // 사진 업로드 버튼
-function uploadImage(carName, title, imageId) {
-    let input = Array.from(inputFileList).filter(ele => ele.title == carName).find(ele => ele.dataset.title == title).files[0];
-    let formData;
+function uploadImage(behavior, carName, title, imageId) {
+    let formData = new FormData();
 
-    if (input) {
-        formData = new FormData();
-        formData.append('data', input);
-    }
-
-    let data = {
-        imageId: imageId,
-        carName: carName,
-        title: title,
-        url: '',
-        isUploaded: 1,
-        file: formData
-    };
-
-    console.log(data);
-
-    if (formData) {
-        if (confirm('해당 사진을 업로드 하시겠습니까?')) {
+    if (behavior == 'mainUpload') {
+        let input = Array.from(inputFileList).filter(ele => ele.title == carName).find(ele => ele.dataset.title == '0').files[0];
+        if (input) {
+            formData.append('file', input);
+            formData.append('imageId', imageId);
+            formData.append('carName', carName);
+            formData.append('title', title);
+            formData.append('url', '');
+            formData.append('isUploaded', '1');
+            formData.append('isMain', '1');
+            if (confirm('해당 사진을 대표 이미지로 등록 하시겠습니까?')) {
+                console.log(formData.getAll('file'));
+                console.log(formData.getAll('imageId'));
+                console.log(formData.getAll('carName'));
+                console.log(formData.getAll('title'));
+                console.log(formData.getAll('url'));
+                console.log(formData.getAll('isUploaded'));
+                console.log(formData.getAll('isMain'));
+                $.ajax({
+                    enctype: 'multipart/form-data',
+                    cache: false,
+                    type: 'POST',
+                    url: '/admin/campingcar/image/',
+                    processData:false,
+                    contentType: false,
+                    data: formData
+                }).done(function (result) {
+                    if (result.result == 1) {
+                        alert('업로드가 완료되었습니다.');
+                        window.location.href = '/admin/campingcar/image/menu';
+                    } else {
+                        alert('업로드에 문제가 생겼습니다.');
+                    };
+                }).fail(function (error) {
+                    alert(JSON.stringify(error));
+                })
+            };
+        } else {
+            alert('업로드할 사진을 선택해주세요.');
+        };
+    } else if (behavior == 'mainDelete') {
+        if (confirm('대표 이미지를 삭제 하시겠습니까?')) {
             $.ajax({
-                enctype: 'multipart/form-data',
-                cache: false,
-                type: 'PUT',
-                url: '/admin/campingcar/image/' + imageId,
-                processData:false,
-                contentType: false,
-                dataType: 'json',
-                data: JSON.stringify(data)
+                type:'DELETE',
+                url:'/admin/campingcar/image/'+ imageId,
+                dataType:'json',
+                contentType : 'application/json; charset=utf-8'
             }).done(function (result) {
                 if (result.result == 1) {
-                    alert('업로드가 완료되었습니다.');
+                    alert('삭제가 완료되었습니다.');
+                    window.location.href = '/admin/campingcar/image/menu';
                 } else {
-                    alert('업로드에 문제가 생겼습니다.');
+                    alert('삭제에 문제가 생겼습니다.');
                 };
-                window.location.href = '/admin/campingcar/image/menu';
             }).fail(function (error) {
                 alert(JSON.stringify(error));
             })
         };
-    } else {
-        alert('업로드할 사진을 선택해주세요.');
-    }
+    };
+
+    // if (behavior == 'upload') {
+    //     let input = Array.from(inputFileList).filter(ele => ele.title == carName).find(ele => ele.dataset.title == title).files[0];
+    //
+    //     if (input) {
+    //         formData.append('file', input);
+    //         formData.append('imageId', imageId);
+    //         formData.append('carName', carName);
+    //         formData.append('title', title);
+    //         formData.append('url', '');
+    //         formData.append('isUploaded', '1');
+    //         if (confirm('해당 사진을 업로드 하시겠습니까?')) {
+    //             sendingData();
+    //         };
+    //     } else {
+    //         alert('업로드할 사진을 선택해주세요.');
+    //     };
+    //
+    // } else if (behavior == 'delete') {
+    //     formData.append('file', '');
+    //     formData.append('imageId', imageId);
+    //     formData.append('carName', carName);
+    //     formData.append('title', title);
+    //     formData.append('url', '');
+    //     formData.append('isUploaded', '0');
+    //     if (confirm('해당 사진을 삭제 하시겠습니까?')) {
+    //         console.log(formData.get('file'));
+    //         console.log(formData.get('imageId'));
+    //         console.log(formData.get('carName'));
+    //         console.log(formData.get('title'));
+    //         console.log(formData.get('url'));
+    //         console.log(formData.get('isUploaded'));
+    //         sendingData();
+    //     };
+    // }
+
+    // console.log(input);
+    // console.log(formData.getAll('data'));
+    //
+    // let data = {
+    //     imageId: imageId,
+    //     carName: carName,
+    //     title: title.toString(),
+    //     url: '',
+    //     isUploaded: '1',
+    //     file: formData
+    // };
+
+
+    // if (formData) {
+    //
+    // } else {
+    // }
+
+    function sendingData() {
+        $.ajax({
+            enctype: 'multipart/form-data',
+            cache: false,
+            type: 'PUT',
+            url: '/admin/campingcar/image/' + imageId,
+            processData:false,
+            contentType: false,
+            data: formData
+        }).done(function (result) {
+            if (result.result == 1) {
+                alert('업로드가 완료되었습니다.');
+                window.location.href = '/admin/campingcar/image/menu';
+            } else {
+                alert('업로드에 문제가 생겼습니다.');
+            };
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        })
+    };
+
 
 }
 // function doNotMove() {
