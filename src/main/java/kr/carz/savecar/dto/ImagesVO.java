a7c4720c0d57a54598a4ef026676e9f719e9edde
sne12345 <sne12345@naver.com>
2022-02-04 16:36:05 +0900

    캠핑카 이미지 PUT 오류 수정

--- src/main/java/kr/carz/savecar/dto/ImagesVO.java
+++ src/main/java/kr/carz/savecar/dto/ImagesVO.java
@@ -2,11 +2,13 @@ package kr.carz.savecar.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ImagesVO {
 
     private int title;
