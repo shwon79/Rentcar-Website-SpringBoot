f499f5cd5f295740954a9f2f48f01ec84b315d50
Jay-JYKim91 <jay.jykim91@gmail.com>
2022-02-04 16:32:45 +0900

    캠핑카 이미지 수정_순서 바꾸기 put 하는 중

--- src/main/resources/static/js/admin_campingcar_image.js
+++ src/main/resources/static/js/admin_campingcar_image.js
@@ -2,36 +2,7 @@ let imgBoxList = document.getElementsByClassName('image-box');
 let uploadBoxList = document.getElementsByClassName('upload-box');
 let inputFileList = document.querySelectorAll('input[type="file"]');
 
-// Array.from(uploadBoxList).forEach(function(uploadBox) {
-//     uploadBox.addEventListener('dragenter', function(e) {
-//         console.log('dragenter');
-//     });
-//
-//     uploadBox.addEventListener('dragover', function(e) {
-//         e.preventDefault();
-//         console.log('dragover');
-//
-//         this.style.backgroundColor = 'lightblue';
-//     });
-//     uploadBox.addEventListener('dragleave', function(e) {
-//         console.log('dragleave');
-//
-//         this.style.backgroundColor = 'white';
-//     });
-//     uploadBox.addEventListener('drop', function(e) {
-//         e.preventDefault();
-//
-//         console.log('drop');
-//         this.style.backgroundColor = 'white';
-//         console.dir(e.dataTransfer);
-//
-//         var data = e.dataTransfer.files[0];
-//         console.dir(data);
-//     });
-// });
-
-
-// 사진 업로드 버튼
+// 메인&추가 이미지 등록 및 삭제
 function uploadImage(behavior, carName, title, imageId) {
     let formData = new FormData();
 
@@ -100,7 +71,7 @@ function uploadImage(behavior, carName, title, imageId) {
         $.ajax({
             type:'DELETE',
             url:'/admin/campingcar/image/'+ imageId,
-            dataType:'json',
+            dataType:'text',
             contentType : 'application/json; charset=utf-8'
         }).done(function () {
             alert('삭제가 완료되었습니다.');
@@ -119,6 +90,8 @@ function uploadImage(behavior, carName, title, imageId) {
 //             processData:false,
 //             contentType: false,
 //             data: formData
+
+//datatype: text
 //         }).done(function (result) {
 //             if (result.result == 1) {
 //                 alert('업로드가 완료되었습니다.');
@@ -129,3 +102,78 @@ function uploadImage(behavior, carName, title, imageId) {
 //         }).fail(function (error) {
 //             alert(JSON.stringify(error));
 //         })
+
+// 이미지 순서 변경 창 보이기
+function displaySortingBox() {
+    let boxList = document.getElementsByClassName('changeOrderBox');
+    [...boxList].forEach(function(box) {
+        box.classList.toggle('active');
+    });
+};
+
+// 이미지 순서 변경 후 저장
+function saveChangedData(carName) {
+    let boxList = document.getElementsByClassName(carName+'Item');
+    // console.log(boxList);
+    // if (confirm('추가 이미지 순서를 변경하시겠습니까?')) {
+    //
+    // }
+    let tempTitle = 1;
+    [...boxList].forEach(function(box) {
+        let imageId = box.dataset.imageid;
+
+        let data = {
+            title: tempTitle
+        }
+
+        console.log(data);
+
+        $.ajax({
+            type: 'PUT',
+            url: '/admin/campingcar/image/' + imageId,
+            dataType: 'json',
+            contentType : 'application/json; charset=utf-8',
+            data : JSON.stringify(data)
+        }).done(function (result) {
+            if (result.result == 1) {
+                alert('변경 되었습니다.');
+            } else {
+                alert('변경에 문제가 생겼습니다.');
+            };
+            // alert('변경이 완료되었습니다.');
+            // window.location.href = '/admin/campingcar/image/menu';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        })
+        tempTitle++;
+    });
+    // window.location.href = '/admin/campingcar/image/menu';
+}
+
+// Array.from(uploadBoxList).forEach(function(uploadBox) {
+//     uploadBox.addEventListener('dragenter', function(e) {
+//         console.log('dragenter');
+//     });
+//
+//     uploadBox.addEventListener('dragover', function(e) {
+//         e.preventDefault();
+//         console.log('dragover');
+//
+//         this.style.backgroundColor = 'lightblue';
+//     });
+//     uploadBox.addEventListener('dragleave', function(e) {
+//         console.log('dragleave');
+//
+//         this.style.backgroundColor = 'white';
+//     });
+//     uploadBox.addEventListener('drop', function(e) {
+//         e.preventDefault();
+//
+//         console.log('drop');
+//         this.style.backgroundColor = 'white';
+//         console.dir(e.dataTransfer);
+//
+//         var data = e.dataTransfer.files[0];
+//         console.dir(data);
+//     });
+// });
