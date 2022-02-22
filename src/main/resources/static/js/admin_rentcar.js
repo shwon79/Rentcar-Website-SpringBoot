04de77d08b96a4562c93932a41126980f2b84edc
Jay-JYKim91 <jay.jykim91@gmail.com>
2022-02-22 17:47:25 +0900

    admin/rentcar/price 가격 수정 진행 중

--- src/main/resources/static/js/admin_rentcar.js
+++ src/main/resources/static/js/admin_rentcar.js
@@ -33,15 +33,11 @@ function displayCategory2(event) {
 };
 
 //월렌트 메뉴 페이지 가격 수정 버튼
-function editMonthlyPriceMenu(id) {
+function editRentPriceMenu(id, period) {
     let category1 = [...document.getElementsByClassName('category1')].find(item => item.dataset.title == id);
     let category2 = [...document.getElementsByClassName('category2')].find(item => item.dataset.title == id);
     let name = [...document.getElementsByClassName('name')].find(item => item.dataset.title == id);
     let deposit = [...document.getElementsByClassName('deposit')].find(item => item.dataset.title == id);
-    let cost_for_2k = [...document.getElementsByClassName('cost_for_2k')].find(item => item.dataset.title == id);
-    let cost_for_2_5k = [...document.getElementsByClassName('cost_for_2_5k')].find(item => item.dataset.title == id);
-    let cost_for_3k = [...document.getElementsByClassName('cost_for_3k')].find(item => item.dataset.title == id);
-    let cost_for_4k = [...document.getElementsByClassName('cost_for_4k')].find(item => item.dataset.title == id);
     let cost_for_others = [...document.getElementsByClassName('cost_for_others')].find(item => item.dataset.title == id);
     let age_limit = [...document.getElementsByClassName('age_limit')].find(item => item.dataset.title == id);
     let cost_per_km = [...document.getElementsByClassName('cost_per_km')].find(item => item.dataset.title == id);
@@ -53,14 +49,10 @@ function editMonthlyPriceMenu(id) {
 
     let data = {
         id: id,
-        category1: category1.value,
-        category2: category2.value,
-        name: name.value,
+        category1: category1.value || category1.innerText,
+        category2: category2.value || category2.innerText,
+        name: name.value || name.innerText,
         deposit: deposit.value.replace(/,/g, ''),
-        cost_for_2k: cost_for_2k.value.replace(/,/g, ''),
-        cost_for_2_5k: cost_for_2_5k.value.replace(/,/g, ''),
-        cost_for_3k: cost_for_3k.value.replace(/,/g, ''),
-        cost_for_4k: cost_for_4k.value.replace(/,/g, ''),
         cost_for_others: cost_for_others.value,
         age_limit: age_limit.innerText,
         cost_per_km: cost_per_km.innerText,
@@ -69,47 +61,185 @@ function editMonthlyPriceMenu(id) {
         end: parseInt(end.innerText),
         credit: credit.innerText,
         img_url: img_url.innerText
+    };
+
+    if (period === 'monthly') {
+        let cost_for_2k = [...document.getElementsByClassName('cost_for_2k')].find(item => item.dataset.title == id);
+        let cost_for_2_5k = [...document.getElementsByClassName('cost_for_2_5k')].find(item => item.dataset.title == id);
+        let cost_for_3k = [...document.getElementsByClassName('cost_for_3k')].find(item => item.dataset.title == id);
+        let cost_for_4k = [...document.getElementsByClassName('cost_for_4k')].find(item => item.dataset.title == id);
+
+        data['cost_for_2k'] = cost_for_2k.value.replace(/,/g, '');
+        data['cost_for_2_5k'] = cost_for_2_5k.value.replace(/,/g, '');
+        data['cost_for_3k'] = cost_for_3k.value.replace(/,/g, '');
+        data['cost_for_4k'] = cost_for_4k.value.replace(/,/g, '');
+
+        postStringData(period, id, data);
+    } else if (period === 'yearly') {
+        let cost_for_20k = [...document.getElementsByClassName('cost_for_20k')].find(item => item.dataset.title == id);
+        let cost_for_30k = [...document.getElementsByClassName('cost_for_30k')].find(item => item.dataset.title == id);
+        let cost_for_40k = [...document.getElementsByClassName('cost_for_40k')].find(item => item.dataset.title == id);
+
+        data['cost_for_20k'] = cost_for_20k.value;
+        data['cost_for_30k'] = cost_for_30k.value;
+        data['cost_for_40k'] = cost_for_40k.value;
+
+        console.log(data);
+
+    } else if (period === 'twoYearly') {
+        let cost_for_20Tk = [...document.getElementsByClassName('cost_for_20Tk')].find(item => item.dataset.title == id);
+        let cost_for_30Tk = [...document.getElementsByClassName('cost_for_30Tk')].find(item => item.dataset.title == id);
+        let cost_for_40Tk = [...document.getElementsByClassName('cost_for_40Tk')].find(item => item.dataset.title == id);
+
+        data['cost_for_20Tk'] = cost_for_20Tk.value;
+        data['cost_for_30Tk'] = cost_for_30Tk.value;
+        data['cost_for_40Tk'] = cost_for_40Tk.value;
+
+        console.log(data);
     }
-    console.log(data);
 }
 
 //월렌트 상세 페이지 가격 수정 버튼
-function editMonthlyPriceDetail(id) {
+function editRentPriceDetail(id, period) {
     const category1 = document.getElementById('category1');
     const category2 = document.getElementById('category2');
     const name = document.getElementById('name');
-    let deposit = document.getElementById('category1');
-    let cost_for_2k = document.getElementById('cost_for_2k');
-    let cost_for_2_5k = document.getElementById('cost_for_2_5k');
-    let cost_for_3k = document.getElementById('cost_for_3k');
-    let cost_for_4k = document.getElementById('cost_for_4k');
+    let deposit = document.getElementById('deposit');
     const cost_for_others = document.getElementById('cost_for_others');
     let age_limit = document.getElementById('age_limit');
     const cost_per_km = document.getElementById('cost_per_km');
     const nameMoren = document.getElementById('nameMoren');
-    const start = document.getElementById('start');
-    const end = document.getElementById('end');
+    const start = parseInt(document.getElementById('start').value);
+    const end = parseInt(document.getElementById('end').value);
     const credit = document.getElementById('credit');
     const img_url = document.getElementById('img_url');
+    const img_input = document.getElementById('img_input').files[0];
 
-    let data = {
-        id: id,
-        category1: category1.value,
-        category2: category2.value,
-        name: name.value,
-        deposit: deposit.value.replace(/,/g, ''),
-        cost_for_2k: cost_for_2k.value.replace(/,/g, ''),
-        cost_for_2_5k: cost_for_2_5k.value.replace(/,/g, ''),
-        cost_for_3k: cost_for_3k.value.replace(/,/g, ''),
-        cost_for_4k: cost_for_4k.value.replace(/,/g, ''),
-        cost_for_others: cost_for_others.value,
-        age_limit: age_limit.value.replace(/,/g, ''),
-        cost_per_km: cost_per_km.value,
-        nameMoren: nameMoren.value,
-        start: parseInt(start.value),
-        end: parseInt(end.value),
-        credit: credit.value,
-        img_url: img_url.src
+    // 이미지를 새로 추가하지 않은 경우
+    if (img_input === undefined) {
+        let data = {
+            id: id,
+            category1: category1.value,
+            category2: category2.value,
+            name: name.value,
+            deposit: deposit.value.replace(/,/g, ''),
+            cost_for_others: cost_for_others.value,
+            age_limit: age_limit.value.replace(/,/g, ''),
+            cost_per_km: cost_per_km.value,
+            nameMoren: nameMoren.value,
+            start: start,
+            end: end,
+            credit: credit.value,
+            img_url: img_url.src
+        }
+
+        if (period === 'monthly') {
+            let cost_for_2k = document.getElementById('cost_for_2k');
+            let cost_for_2_5k = document.getElementById('cost_for_2_5k');
+            let cost_for_3k = document.getElementById('cost_for_3k');
+            let cost_for_4k = document.getElementById('cost_for_4k');
+
+            data['cost_for_2k'] = cost_for_2k.value.replace(/,/g, '');
+            data['cost_for_2_5k'] = cost_for_2_5k.value.replace(/,/g, '');
+            data['cost_for_3k'] = cost_for_3k.value.replace(/,/g, '');
+            data['cost_for_4k'] = cost_for_4k.value.replace(/,/g, '');
+
+            postStringData(period, id, data);
+        } else if (period === 'yearly') {
+            let cost_for_20k = document.getElementById('cost_for_20k');
+            let cost_for_30k = document.getElementById('cost_for_30k');
+            let cost_for_40k = document.getElementById('cost_for_40k');
+
+            data['cost_for_20k'] = cost_for_20k.value.replace(/,/g, '');
+            data['cost_for_30k'] = cost_for_30k.value.replace(/,/g, '');
+            data['cost_for_40k'] = cost_for_40k.value.replace(/,/g, '');
+            console.log(data);
+
+        } else if (period === 'twoYearly') {
+            let cost_for_20Tk = document.getElementById('cost_for_20Tk');
+            let cost_for_30Tk = document.getElementById('cost_for_30Tk');
+            let cost_for_40Tk = document.getElementById('cost_for_40Tk');
+
+            data['cost_for_20Tk'] = cost_for_20Tk.value.replace(/,/g, '');
+            data['cost_for_30Tk'] = cost_for_30Tk.value.replace(/,/g, '');
+            data['cost_for_40Tk'] = cost_for_40Tk.value.replace(/,/g, '');
+
+            console.log(data);
+        }
+
+    } else {
+        // 이미지를 새로 추가한 경우
+        let formData = new FormData();
+
+        formData.append('id', id);
+        formData.append('category1', category1.value);
+        formData.append('category2', category2.value);
+        formData.append('name', name.value);
+        formData.append('deposit', deposit.value.replace(/,/g, ''));
+        formData.append('cost_for_others', cost_for_others.value);
+        formData.append('age_limit', age_limit.value.replace(/,/g, ''));
+        formData.append('cost_per_km', cost_per_km.value);
+        formData.append('nameMoren', nameMoren.value);
+        formData.append('start', start);
+        formData.append('end', end);
+        formData.append('credit', credit.value);
+        formData.append('img_url', img_input);
+
+        if (period === 'monthly') {
+            let cost_for_2k = document.getElementById('cost_for_2k');
+            let cost_for_2_5k = document.getElementById('cost_for_2_5k');
+            let cost_for_3k = document.getElementById('cost_for_3k');
+            let cost_for_4k = document.getElementById('cost_for_4k');
+
+            formData.append('cost_for_2k', cost_for_2k.value.replace(/,/g, ''));
+            formData.append('cost_for_2_5k', cost_for_2_5k.value.replace(/,/g, ''));
+            formData.append('cost_for_3k', cost_for_3k.value.replace(/,/g, ''));
+            formData.append('cost_for_4k', cost_for_4k.value.replace(/,/g, ''));
+
+        } else if (period === 'yearly') {
+            let cost_for_20k = document.getElementById('cost_for_20k');
+            let cost_for_30k = document.getElementById('cost_for_30k');
+            let cost_for_40k = document.getElementById('cost_for_40k');
+
+            formData.append('cost_for_20k', cost_for_20k.value.replace(/,/g, ''));
+            formData.append('cost_for_30k', cost_for_30k.value.replace(/,/g, ''));
+            formData.append('cost_for_40k', cost_for_40k.value.replace(/,/g, ''));
+
+        } else if (period === 'twoYearly') {
+            let cost_for_20Tk = document.getElementById('cost_for_20Tk');
+            let cost_for_30Tk = document.getElementById('cost_for_30Tk');
+            let cost_for_40Tk = document.getElementById('cost_for_40Tk');
+
+            formData.append('cost_for_20Tk', cost_for_20Tk.value.replace(/,/g, ''));
+            formData.append('cost_for_30Tk', cost_for_30Tk.value.replace(/,/g, ''));
+            formData.append('cost_for_40Tk', cost_for_40Tk.value.replace(/,/g, ''));
+
+        }
+
+        for (let key of formData.keys()) {
+            console.log(key);
+        }
+        for (let value of formData.values()) {
+            console.log(value);
+        }
     }
-    console.log(data);
+}
+
+function postStringData(period, id, data) {
+        $.ajax({
+            type:'PUT',
+            url:'/admin/rentcar/price/' + period + '/' + id,
+            dataType:'json',
+            contentType : 'application/json; charset=utf-8',
+            data : JSON.stringify(data)
+        }).done(function (result) {
+            if (result.result == 1) {
+                alert('처리되었습니다.');
+            } else {
+                alert('처리에 문제가 생겼습니다.');
+            };
+            location.reload();
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        })
 }
