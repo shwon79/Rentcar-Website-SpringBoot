fcd736a6d7306d72bda2782e719f65d84ba6f7fb
sne12345 <sne12345@naver.com>
2022-03-11 19:41:29 +0900

    누구나 간편 상담신청 ajax 수정

--- src/main/resources/static/js/rent_long_term.js
+++ src/main/resources/static/js/rent_long_term.js
@@ -13,8 +13,8 @@ let smallBox = document.getElementsByClassName('small_image_box');
     });
 });
 
-// 월렌트실시간 상담요청
-function make_monthly_rent_reservation (e) {
+// 누구나간편 상담요청
+function make_long_term_rent_reservation (e) {
 
     if (document.getElementById("reservation-simple-name").value == ""){
         alert('성함을 입력해주세요.')
@@ -26,7 +26,7 @@ function make_monthly_rent_reservation (e) {
         return
     }
 
-
+    let reservationPhone = $("#reservation-simple-phone").val();
     let regPhone = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
 
 
@@ -34,7 +34,7 @@ function make_monthly_rent_reservation (e) {
         name : $("#reservation-simple-name").val(),
         phoneNo : reservationPhone,
         detail : $("#reservation-simple-details").val(),
-        product: '장기렌트',
+        product: '누구나 장기렌트',
         title: '누구나간편',
         category1: '',
         category2: '',
@@ -50,7 +50,7 @@ function make_monthly_rent_reservation (e) {
         carAge: ''
     };
 
-    // console.log(data);
+    console.log(data);
 
     let check1 = document.getElementById("agree1").checked;
     let check2 = document.getElementById("agree2").checked;
