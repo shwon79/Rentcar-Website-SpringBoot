f27981d8ed3b1e24624015288bda0a282c3bea44
Jay-JYKim91 <jay.jykim91@gmail.com>
2022-03-16 16:32:16 +0900

    /rent/estimate 디폴트값 보이게 수정 & 코드 정리

--- src/main/resources/static/js/monthly_rent.js
+++ src/main/resources/static/js/monthly_rent.js
@@ -1,44 +1,20 @@
-function erase_after_catergory1(){
-
-    document.getElementById('select-category1').value = "";
-    document.getElementById('select-category2').value = "";
-    document.getElementById('select-car-name').value = "";
-    document.getElementById('select-mileage').value = "";
-    document.getElementById("carPrice").innerText = "원";
-    document.getElementById("carVat").innerText = "원";
-    document.getElementById("carDeposit").innerText = "원";
-    document.getElementById("carTotal").innerText = "원";
-    setSelectBoxByText('select-category2', "차 분류를 선택해주세요");
-    setSelectBoxByText('select-car-name', "차명을 선택해주세요");
-    setSelectBoxByText('select-mileage', "주행거리 선택해주세요");
-
-}
-
-function erase_after_catergory2(){
-
-    document.getElementById('select-category2').value = "";
-    document.getElementById('select-car-name').value = "";
-    document.getElementById('select-mileage').value = "";
-    document.getElementById("carPrice").innerText = "원";
-    document.getElementById("carVat").innerText = "원";
-    document.getElementById("carDeposit").innerText = "원";
-    document.getElementById("carTotal").innerText = "원";
-    setSelectBoxByText('select-car-name', "차명을 선택해주세요");
-    setSelectBoxByText('select-mileage', "주행거리 선택해주세요");
-}
-
-function erase_after_carname(){
-
-    document.getElementById('select-car-name').value = "";
-    document.getElementById('select-mileage').value = "";
-    document.getElementById("carPrice").innerText = "원";
-    document.getElementById("carVat").innerText = "원";
-    document.getElementById("carDeposit").innerText = "원";
-    document.getElementById("carTotal").innerText = "원";
-    setSelectBoxByText('select-mileage', "주행거리 선택해주세요");
-}
-
-function erase_after_milege(){
+function erase_after(target) {
+    if (target === 'category1') {
+        document.getElementById('select-category1').value = "";
+        document.getElementById('select-category2').value = "";
+        document.getElementById('select-car-name').value = "";
+        setSelectBoxByText('select-category2', "차 분류를 선택해주세요");
+        setSelectBoxByText('select-car-name', "차명을 선택해주세요");
+        setSelectBoxByText('select-mileage', "주행거리 선택해주세요");
+    } else if (target === 'category2') {
+        document.getElementById('select-category2').value = "";
+        document.getElementById('select-car-name').value = "";
+        setSelectBoxByText('select-car-name', "차명을 선택해주세요");
+        setSelectBoxByText('select-mileage', "주행거리 선택해주세요");
+    } else if (target === 'carname') {
+        document.getElementById('select-car-name').value = "";
+        setSelectBoxByText('select-mileage', "주행거리 선택해주세요");
+    }
 
     document.getElementById('select-mileage').value = "";
     document.getElementById("carPrice").innerText = "원";
@@ -49,19 +25,18 @@ function erase_after_milege(){
 
 // 간편상담 요청
 function make_easy_reservation () {
-
     if (document.getElementById("reservation-detail-name").value == ""){
-        alert('성함을 입력해주세요.')
-        return
+        alert('성함을 입력해주세요.');
+        return;
     }
 
     if (document.getElementById("reservation-detail-phone").value == ""){
-        alert('전화번호를 입력해주세요.')
-        return
+        alert('전화번호를 입력해주세요.');
+        return;
     }
 
 
-    var data = {
+    let data = {
         name : $("#reservation-detail-name").val(),
         phoneNo : $("#reservation-detail-phone").val(),
         detail : $("#reservation-detail-details").val(),
@@ -71,7 +46,8 @@ function make_easy_reservation () {
         resDate : $("#reservation-detail-resdate").val()
     };
 
-    var checkbox = document.getElementById("agree")
+    let checkbox = document.getElementById("agree");
+
     if(checkbox.checked) {
         $.ajax({
             type : 'POST',
@@ -104,7 +80,7 @@ function make_reservation () {
         return
     }
 
-    var product;
+    let product;
     if (document.getElementById("long-rent-product1") != null) {
 
         if (document.getElementById("long-rent-product1").checked) {
@@ -117,8 +93,8 @@ function make_reservation () {
 
     } else if (document.getElementById("rent-product1") != null) {
 
-        var month1 = document.getElementById("rent-product1")
-        var yearly = document.getElementById("rent-product2")
+        let month1 = document.getElementById("rent-product1")
+        let yearly = document.getElementById("rent-product2")
 
         if (month1 && month1.checked) {
             product = document.getElementById("rent-product1").getAttribute("value")
@@ -142,7 +118,7 @@ function make_reservation () {
     }
 
 
-    var deposit;
+    let deposit;
     // 누구나 장기렌트
     if (document.getElementById("deposit-10") != null) {
         if (document.getElementById("deposit-10").checked){
@@ -159,20 +135,20 @@ function make_reservation () {
 
 
 
-    var price;
+    let price;
     if (document.getElementById("carTotal") != null) {
         price = document.getElementById("carTotal").innerText;
     }
 
 
-    var age_limit;
+    let age_limit;
     if (document.getElementById("age_limit").checked){
         age_limit = "만 21세 이상~만 26세 미만"
     } else {
         age_limit = "만 26세 이상"
     }
 
-    var data = {
+    let data = {
         name : $("#reservation-detail-name").val(),
         phoneNo : $("#reservation-detail-phone").val(),
         detail : $("#reservation-detail-details").val(),
@@ -190,7 +166,7 @@ function make_reservation () {
 
 
 
-    var checkbox = document.getElementById("agree")
+    let checkbox = document.getElementById("agree")
     if(checkbox.checked) {
         $.ajax({
             type : 'POST',
@@ -210,37 +186,18 @@ function make_reservation () {
 }
 
 //차종 구하기
-const get_category1 = (fr, detailedSelect) => {
-  fetch(`/rent/estimate/${fr}`)
-      .then(response => response.json())
-      .then(result => {
-          for (let i = 0; i < result.length; i++) {
-              detailedSelect.options[i+1] = new Option(result[i], result[i]);
-          }
-      }).catch(err => console.log(err))
-}
-
-/*
 function get_category1(fr, detailedSelect) {
-    $.ajax({
-        type: 'GET',
-        url: '/rent/month/' + fr,
-        contentType: "application/json; charset=UTF-8",
-        dataType: 'json',
-        success: function set_c1(result) {
-            for (i = 0; i < result.length; i++) {
+    fetch(`/rent/estimate/${fr}`)
+        .then(response => response.json())
+        .then(result => {
+            for (let i = 0; i < result.length; i++) {
                 detailedSelect.options[i+1] = new Option(result[i], result[i]);
             }
-        }
-    }).fail(function (error) {
-        alert(JSON.stringify(error));
-    })
-}
- */
+        }).catch(err => console.log(err))
+};
 
 //차 분류 구하기
-const get_category2 = (fr1, fr2, detailedSelect) => {
-
+function get_category2(fr1, fr2, detailedSelect) {
     fetch(`/rent/estimate/${fr1}/${fr2}`)
         .then(response => response.json())
         .then(result => {
@@ -248,29 +205,11 @@ const get_category2 = (fr1, fr2, detailedSelect) => {
             for (let i = 0; i < result.length; i++) {
                 detailedSelect.options[i+1] = new Option(result[i], result[i]);
             }
-        }).catch(err => console.log(err))
-
-    /*
-    $.ajax({
-        type: 'GET',
-        url: '/rent/month/' + fr1 + '/' + fr2,
-        contentType: "application/json; charset=UTF-8",
-        dataType: 'json',
-        success: function set_c2(result) {
-
-            detailedSelect.options.length = 1;
-            for (i = 0; i < result.length; i++) {
-                detailedSelect.options[i+1] = new Option(result[i], result[i]);
-            }
-        }
-    }).fail(function (error) {
-        alert(JSON.stringify(error));
-    })
-     */
+        }).catch(err => console.log(err));
 }
-// 차명 구하기
-const get_car_name = (fr1, fr2, fr3, detailedSelect) => {
 
+// 차명 구하기
+function get_car_name(fr1, fr2, fr3, detailedSelect) {
     fetch(`/rent/estimate/${fr1}/name/${fr2}/${fr3}`)
         .then(response => response.json())
         .then(result => {
@@ -278,27 +217,12 @@ const get_car_name = (fr1, fr2, fr3, detailedSelect) => {
             for (let i = 0; i < result.length; i++) {
                 detailedSelect.options[i+1] = new Option(result[i], result[i]);
             }
-        }).catch(err=>console.log(err))
+        }).catch(err=>console.log(err));
+};
 
-    /*
-    $.ajax({
-        type: 'GET',
-        url: '/rent/month/' + fr1 + "/name/" + fr2 + '/' + fr3,
-        contentType: "application/json; charset=UTF-8",
-        dataType: 'json',
-        success: function set_n(result) {
-            detailedSelect.options.length = 1;
-            for (i = 0; i < result.length; i++) {
-                detailedSelect.options[i+1] = new Option(result[i], result[i]);
-            }
-        }
-    }).fail(function (error) {
-        alert(JSON.stringify(error));
-    })
-     */
-}
 //주행거리 구하기
-const get_mileage = (fr1, detailedSelect) => {
+function get_mileage(fr1, detailedSelect) {
+    let mileage_options;
     detailedSelect.length = 1;
     if (fr1 == "rentMonth") {
         mileage_options = [2000, 2500, 3000, 4000, "기타주행거리"];
@@ -309,6 +233,7 @@ const get_mileage = (fr1, detailedSelect) => {
         detailedSelect.options[i+1] = new Option(mileage_options[i], mileage_options[i]);
     }
 }
+
 // 가격 파싱해서 천단위마다 , 로 끊기
 const int_to_price = (price) => {
     let len = price.length;
@@ -324,8 +249,9 @@ const int_to_price = (price) => {
     result = price.slice(0, len%3) + result;
     return result;
 }
+
 // 요청한 값들에 따라 가격 구하기
-const get_price = (fr1, fr2, fr3, detailedSelect) => {
+function get_price(fr1, fr2, fr3, detailedSelect) {
 
     fetch(`/rent/estimate/${fr1}/price/${fr2}/${fr3}`)
         .then((response) => response.json())
@@ -364,66 +290,16 @@ const get_price = (fr1, fr2, fr3, detailedSelect) => {
                 document.getElementById("carDeposit").innerText = deposit +"원";
                 document.getElementById("carTotal").innerText =  total +"원";
             }
-        }).catch(err => {console.log(err)})
-
-    /*
-    $.ajax({
-        type: 'GET',
-        url: '/rent/month/' + fr1 + '/price/' + fr2 + '/' + fr3,
-        contentType: "application/json; charset=euc-kr",
-        dataType: 'json',
-        success: function set_p(result) {
-            var price = result[0];
-            var age_limit = result[2];
-
-            if(price == '상담') {
-                var vat = price
-                var deposit = result[1];
-                var total = vat;
-
-                document.getElementById("carPrice").innerText = price;
-                document.getElementById("carVat").innerText = vat;
-                document.getElementById("carDeposit").innerText = deposit +"원";
-                document.getElementById("carTotal").innerText =  total;
-
-            } else {
-
-                if(document.getElementById("age_limit").checked) {
-                    price = parseInt(price) + parseInt(age_limit);
-                } else {
-                    price = parseInt(price)
-                }
-
-                // var price_no_comma = price.replace(/,/gi, "");
-                vat = price * 0.1;
-                var total = price + vat;
-                var deposit = result[1];
-
-                price = int_to_price(price.toString());
-                vat = int_to_price(vat.toString());
-                total = int_to_price(total.toString());
-
-                document.getElementById("carPrice").innerText = price +"원";
-                document.getElementById("carVat").innerText = vat +"원";
-                document.getElementById("carDeposit").innerText = deposit +"원";
-                document.getElementById("carTotal").innerText =  total +"원";
-            }
-
-
-        }
-    }).fail(function (error) {
-        // alert(JSON.stringify(error));
-    })
-    */
+        }).catch(err => {console.log(err)});
 }
 
-function setSelectBoxByText(eid, etxt) {
-    var eid = document.getElementById(eid);
+function setSelectBoxByText(eid, eTxt) {
+    let target = document.getElementById(eid);
 
-    for (var i = 0; i < (eid.options.length); ++i) {
-        if (eid.options[i].innerText == etxt) {
-            eid.options[i].selected = true;
+    for (let i = 0; i < target.options.length; i++) {
+        if (target.options[i].innerText == eTxt) {
+            target.options[i].selected = true;
             break;
         }
-    }
+    };
 }
