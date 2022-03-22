1c3ae0f880bd333dd773388a3064ae088705d43f
sne12345 <sne12345@naver.com>
2022-03-22 16:51:32 +0900

    @PostMapping("/admin/longTerm")

--- src/main/java/kr/carz/savecar/controller/Admin/LongTermRentCarController.java
+++ src/main/java/kr/carz/savecar/controller/Admin/LongTermRentCarController.java
@@ -2,14 +2,26 @@ package kr.carz.savecar.controller.Admin;
 
 import kr.carz.savecar.domain.LongTermRent;
 import kr.carz.savecar.domain.LongTermRentImage;
+import kr.carz.savecar.domain.ValuesForWeb;
+import kr.carz.savecar.dto.LongTermRentDTO;
+import kr.carz.savecar.dto.ValuesForWebDTO;
+import kr.carz.savecar.dto.ValuesVO;
 import kr.carz.savecar.service.*;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class LongTermRentCarController {
@@ -34,6 +46,22 @@ public class LongTermRentCarController {
 
         return "admin/longTerm_main";
     }
+
+    @PostMapping("/admin/longTerm")
+    @ResponseBody
+    public void postAdminLongTerm(HttpServletResponse res, @RequestBody LongTermRentDTO longTermRentDTO) throws IOException {
+
+        longTermRentService.saveDTO(longTermRentDTO);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("result", 1);
+
+        PrintWriter pw = res.getWriter();
+        pw.print(jsonObject);
+        pw.flush();
+        pw.close();
+    }
+
     @GetMapping("/admin/longTerm/detail/{longTermId}")
     public String rent_long_term_detail() {
 
--- src/main/java/kr/carz/savecar/service/LongTermRentService.java
+++ src/main/java/kr/carz/savecar/service/LongTermRentService.java
@@ -1,6 +1,7 @@
 package kr.carz.savecar.service;
 
 import kr.carz.savecar.domain.LongTermRent;
+import kr.carz.savecar.dto.LongTermRentDTO;
 import kr.carz.savecar.repository.LongTermRentRepository;
 
 import java.util.List;
@@ -20,5 +21,9 @@ public class LongTermRentService {
 
     public Optional<LongTermRent> findById(Long carId){
         return longTermRentRepository.findById(carId);
+    }
+
+    public Long saveDTO(LongTermRentDTO dto){
+        return longTermRentRepository.save(dto.toEntity()).getLongTermRentId();
     }
 }
