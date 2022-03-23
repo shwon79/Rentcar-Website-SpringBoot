e9254a398a64cb1b9b2e38b1966b6e6a7f7f0db7
Jay-JYKim91 <jay.jykim91@gmail.com>
2022-03-23 19:00:12 +0900

    /camping/calendar/review 이미지 추가 등록 및 한장씩 삭제

--- src/main/resources/static/js/campingcar.js
+++ src/main/resources/static/js/campingcar.js
@@ -77,6 +77,190 @@ function submitReview() {
     };
 }
 
+// 비밀번호 확인
+function checkPassword(id) {
+    const password = [...document.getElementsByClassName('password')].find(ele => ele.dataset.id == id).innerText;
+    let enterPassword = prompt(`비밀번호를 입력하세요.`, '');
+
+    if (enterPassword === password) {
+        return true;
+    } else {
+        return false;
+    };
+};
+
+// 리뷰 수정하기 창 띄우기
+function displayReviewEditBox(type) {
+    let modal = document.getElementById('reviewEditModal');
+    // let span = document.getElementsByClassName("modal_close_btn")[0];
+
+    if (type == 'open') {
+        modal.style.display = "block";
+    } else if (type == 'close') {
+        modal.style.display = "none";
+        sessionStorage.setItem('camping_review_edit_open', '0');
+        sessionStorage.setItem('review_id', '0');
+    };
+};
+
+// 리뷰 수정하기 창에 기존 입력 값 보여주기
+function displayExistingValue(id) {
+    let nickName = [...document.getElementsByClassName('nickName')].find(ele => ele.dataset.id == id).innerText;
+    let password = [...document.getElementsByClassName('password')].find(ele => ele.dataset.id == id).innerText;
+    let carName = [...document.getElementsByClassName('carName')].find(ele => ele.dataset.id == id).innerText;
+    let startDate = [...document.getElementsByClassName('startDate')].find(ele => ele.dataset.id == id).innerText;
+    let endDate = [...document.getElementsByClassName('endDate')].find(ele => ele.dataset.id == id).innerText;
+    let text = [...document.getElementsByClassName('text')].find(ele => ele.dataset.id == id).innerText;
+    let imageUrlList = [...document.getElementsByClassName('imgUrl')].filter(ele => ele.dataset.id == id);
+    let selectedImageUrlList = [];
+
+    for(let i = 0; i < imageUrlList.length; i++) {
+        selectedImageUrlList.push(imageUrlList[i]);
+    };
+
+    document.getElementById('editNickName').value = nickName;
+    document.getElementById('editPassword').value = password;
+    document.getElementById('editCarName').value = carName;
+    document.getElementById('editStartDate').value = startDate;
+    document.getElementById('editEndDate').value = endDate;
+    document.getElementById('editText').value = text;
+    document.getElementById('reviewIdForEdit').innerText = id;
+
+    document.getElementsByClassName('existing-image-wrapper')[0].innerHTML = '';
+    document.getElementsByClassName('existing-image-wrapper')[0].style.marginBottom = '20px';
+
+    if (selectedImageUrlList.length == 0) {
+        document.getElementsByClassName('existing-image-wrapper')[0].innerHTML = `
+         <p style="text-align: center; width: 100%; font-size: 14px;">없음</p>
+        `;
+
+        document.getElementsByClassName('existing-image-wrapper')[0].style.border = '1px solid lightgray';
+        document.getElementsByClassName('existing-image-wrapper')[0].style.padding = '10px';
+    } else {
+        for(let i = 0; i < selectedImageUrlList.length; i++) {
+            let imageDiv = document.createElement('div');
+            imageDiv.classList.add('reviewEditImageDiv');
+
+            let button = document.createElement('button');
+            let buttonText = document.createTextNode('X');
+            button.appendChild(buttonText);
+            button.classList.add('delete_image_button');
+            button.onclick = function clickedDelete() {
+                deleteOneImage(selectedImageUrlList[i].dataset.imageId, selectedImageUrlList[i].dataset.id);
+            };
+
+            imageDiv.append(button);
+
+            let image = document.createElement('img');
+            image.src = `${selectedImageUrlList[i].src}`;
+            imageDiv.append(image);
+
+            document.getElementsByClassName('existing-image-wrapper')[0].append(imageDiv);
+            document.getElementsByClassName('existing-image-wrapper')[0].style.border = 'none';
+            document.getElementsByClassName('existing-image-wrapper')[0].style.padding = '0px';
+        }
+    }
+
+
+}
+
+// 새로운 이미지 첨부
+function editNewReviewImage() {
+    let newFiles = document.getElementById('addImage').files;
+    let reviewId = document.getElementById('reviewIdForEdit').innerText;
+    let formData = new FormData();
+
+    if (newFiles.length == 0) {
+        alert('새로운 이미지를 선택해주세요.')
+    } else {
+        formData.append('reviewId', reviewId);
+        for (let i = 0; i < newFiles.length; i++) {
+            formData.append('file', newFiles[i]);
+        };
+
+        sessionStorage.setItem('camping_review_edit_open', '1');
+        sessionStorage.setItem('review_id', reviewId);
+
+        if ('이미지를 추가하시겠습니까?') {
+            $.ajax({
+                enctype: 'multipart/form-data',
+                cache: false,
+                type: 'POST',
+                url: '/camping/calendar/review/image',
+                processData:false,
+                contentType: false,
+                data: formData
+            }).done(function () {
+                alert('새로운 이미지가 추가 되었습니다.');
+                location.reload();
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            })
+        };
+    };
+
+}
+
+// 리뷰 수정에서 이미지 하나씩 삭제
+function deleteOneImage(imageId, reviewId) {
+    if (confirm('이 이미지를 삭제하시겠습니까?')) {
+
+        sessionStorage.setItem('camping_review_edit_open', '1');
+        sessionStorage.setItem('review_id', reviewId);
+
+        $.ajax({
+            type:'DELETE',
+            url:'/camping/calendar/review/image/'+ imageId,
+            dataType:'text',
+            contentType : 'application/json; charset=utf-8'
+        }).done(function () {
+            alert('삭제가 완료되었습니다.');
+            location.reload();
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    };
+};
+
+// 리뷰 수정하기 버튼
+function editReview(id) {
+    let resultCheckPassword = checkPassword(id);
+
+    if (resultCheckPassword) {
+        displayReviewEditBox('open');
+        displayExistingValue(id);
+    } else {
+        alert('비밀번호가 틀렸습니다.');
+   };
+};
+
+// 리뷰 삭제하기 버튼
+function deleteReview(id) {
+    let resultCheckPassword = checkPassword(id);
+
+    if (resultCheckPassword) {
+
+    } else {
+        alert('비밀번호가 틀렸습니다.');
+    };
+
+        // $.ajax({
+    //     type:'DELETE',
+    //     url:'/admin/rentcar/price/'+ id,
+    //     dataType:'json',
+    //     contentType : 'application/json; charset=utf-8',
+    // }).done(function (result) {
+    //     if (result.result == 1) {
+    //         alert('삭제 되었습니다.');
+    //     } else if (result.result == 0) {
+    //         alert('삭제에 문제가 생겼습니다.');
+    //     };
+    //     location.reload();
+    // }).fail(function (error) {
+    //     alert(JSON.stringify(error));
+    // })
+}
+
 // 리뷰 클릭하면 크게 보여지도록
 let oneReview = document.getElementsByClassName('one_review');
 oneReview && [...oneReview].forEach((review) => {
@@ -103,7 +287,7 @@ function changeBtnText(reviewId, boolean) {
     const targetCloseBtn = [...document.getElementsByClassName('see_more_btn_close')].find((btn) => btn.dataset.id === reviewId);
 
     if (boolean) {
-        if (targetReview.classList.contains('opened')) {
+        if (targetReview && targetReview.classList.contains('opened')) {
             targetOpenBtn.style.display = 'block';
             targetCloseBtn.style.display = 'none';
         } else {
@@ -111,12 +295,16 @@ function changeBtnText(reviewId, boolean) {
             targetCloseBtn.style.display = 'block';
         }
     } else {
-        if (targetReview.classList.contains('opened')) {
-            targetOpenBtn.style.display = 'none';
-            targetCloseBtn.style.display = 'block';
+        if (targetReview && targetReview.classList.contains('opened')) {
+            if (targetOpenBtn && targetCloseBtn) {
+                targetOpenBtn.style.display = 'none';
+                targetCloseBtn.style.display = 'block';
+            };
         } else {
-            targetOpenBtn.style.display = 'block';
-            targetCloseBtn.style.display = 'none';
+            if (targetOpenBtn && targetCloseBtn) {
+                targetOpenBtn.style.display = 'block';
+                targetCloseBtn.style.display = 'none';
+            };
         }
     }
 };
