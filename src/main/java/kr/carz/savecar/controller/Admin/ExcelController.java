a01a49422231179865912424f339b5d4d2f8c14d
naeun-garden-5 <sne12345@naver.com>
2022-07-04 16:35:03 +0900

    ADD excel dependencies

---
+++ src/main/java/kr/carz/savecar/controller/Admin/ExcelController.java
@@ -0,0 +1,71 @@
+package kr.carz.savecar.controller.Admin;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.carz.savecar.domain.ExcelData;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class ExcelController {
+
+    @GetMapping("/excel")
+    public String main() { // 1
+        return "admin/excel";
+    }
+
+
+    @PostMapping("/excel/read")
+    public String readExcel(@RequestParam("file") MultipartFile file, Model model)
+            throws IOException { // 2
+
+        List<ExcelData> dataList = new ArrayList<>();
+
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename()); // 3
+
+        if (!extension.equals("xlsx") && !extension.equals("xls")) {
+            throw new IOException("엑셀파일만 업로드 해주세요.");
+        }
+
+        Workbook workbook = null;
+
+        if (extension.equals("xlsx")) {
+            workbook = new XSSFWorkbook(file.getInputStream());
+        } else if (extension.equals("xls")) {
+            workbook = new HSSFWorkbook(file.getInputStream());
+        }
+
+        Sheet worksheet = workbook.getSheetAt(0);
+
+        for (int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) { // 4
+
+            Row row = worksheet.getRow(i);
+
+            ExcelData data = new ExcelData();
+
+            data.setNum((int) row.getCell(0).getNumericCellValue());
+            data.setName(row.getCell(1).getStringCellValue());
+            data.setEmail(row.getCell(2).getStringCellValue());
+
+            dataList.add(data);
+        }
+
+        model.addAttribute("datas", dataList); // 5
+
+        return "admin/excelList";
+
+    }
+}
---
+++ src/main/java/kr/carz/savecar/domain/ExcelData.java
@@ -0,0 +1,13 @@
+package kr.carz.savecar.domain;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExcelData {
+
+    private Integer num;
+    private String name;
+    private String email;
+
+}
